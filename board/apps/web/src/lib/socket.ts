7f61d6b8926e1215c503ff10dbf82a155bc5029e
Hyoungjoo Jin <hyoungjoo.j@gmail.com>
2025-12-09 19:54:04 +0900

    feat: add websocket connections between the client and server

---
+++ board/apps/web/src/lib/socket.ts
@@ -0,0 +1,33 @@
+import SockJS from "sockjs-client";
+import { Client } from "@stomp/stompjs";
+import env from "./env";
+import { isBrowser } from "./server";
+import logger from "./logger";
+
+let client: Client | null = null;
+
+export function getStompClient(): Client {
+  if (!isBrowser()) {
+    throw new Error("getStompClient can only be called in the browser");
+  }
+
+  if (client) {
+    return client;
+  }
+
+  const url = env.NEXT_PUBLIC_WEBSOCKET_URL;
+
+  client = new Client({
+    webSocketFactory: () => {
+      return new SockJS(url);
+    },
+    debug: (message) => {
+      logger.debug(`[STOMP] ${message}`);
+    },
+    reconnectDelay: 5000,
+    heartbeatIncoming: 4000,
+    heartbeatOutgoing: 4000,
+  });
+
+  return client;
+}
